f4c39a6d05b44db91305b50dd988e70e7c28c246
abhishekkoranga <abhishekkumar.sneha@gmail.com>
2024-01-04 20:41:56 +0530

    init project

---
+++ src/test/java/com/speer/assessmet/notes/NotesAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.speer.assessmet.notes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotesAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
